389f5405c5acf8fc16c422832592d5e1a626ce67
ShivamKumarJha <jha.shivam3@gmail.com>
2020-03-14 18:26:37 +0000

    full_k61v1_64_bsp-user 8.1.0 O11019 1582031891 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/vivo/1812
+BOARD_VENDOR := vivo
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/vivo/1812/BoardConfigVendor.mk
